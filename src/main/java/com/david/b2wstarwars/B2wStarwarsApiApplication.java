cb5449a5da34fc88c8282d80f87ac991638d5f33
DavidLuna0 <ddavidr569@gmail.com>
2020-10-21 21:04:19 -0300

    First commit

---
+++ src/main/java/com/david/b2wstarwars/B2wStarwarsApiApplication.java
@@ -0,0 +1,13 @@
+package com.david.b2wstarwars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class B2wStarwarsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(B2wStarwarsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/david/b2wstarwars/B2wStarwarsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.david.b2wstarwars;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class B2wStarwarsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
