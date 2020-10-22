7bc76b45d97393733136b58bcb6704bc2ea6a973
DavidLuna0 <ddavidr569@gmail.com>
2020-10-22 11:22:48 -0300

    [chore] - Adding planet quantity to planet returns

--- src/main/java/com/david/b2wstarwars/services/PlanetService.java
+++ src/main/java/com/david/b2wstarwars/services/PlanetService.java
@@ -16,20 +16,48 @@ public class PlanetService {
 	@Autowired
 	PlanetRepository planetRepository;
 	
+	@Autowired
+	SWAPIService swapiService;
+	
 	public Planet findById(String id) {
 		Optional<Planet> obj = planetRepository.findById(id);
+		int filmsQtd = 0;
+		try {
+			filmsQtd = swapiService.getPlanetQuantity(obj.get().getNome());
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		obj.get().setQuantidadeFilmes(filmsQtd);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! - Id: " + id + " Tipo: " + Planet.class.getName()));
 	}
 	
 	public Planet findByNome(String nome) {
 		Optional<Planet> obj = planetRepository.findById(nome);
+		int filmsQtd = 0;
+		try {
+			filmsQtd = swapiService.getPlanetQuantity(obj.get().getNome());
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		obj.get().setQuantidadeFilmes(filmsQtd);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! - Id: " + nome + " Tipo: " + Planet.class.getName()));
 	}
 	
 	public List<Planet> findAll() {
 		List<Planet> objs = planetRepository.findAll();
+		int filmsQtd = 0;
+		for (Planet planet : objs) {
+			try {
+				filmsQtd = swapiService.getPlanetQuantity(planet.getNome());
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			planet.setQuantidadeFilmes(filmsQtd);
+		}
 		return objs;
 	}
 	
